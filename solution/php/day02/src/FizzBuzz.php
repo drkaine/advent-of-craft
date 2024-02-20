ccb18fc690df445f7ce3323941288da8ac85a2b5
drkaine <duretkevin@live.fr>
2024-02-20 19:15:33 +0000

    Resolution of day 02

--- solution/php/day02/src/FizzBuzz.php
+++ solution/php/day02/src/FizzBuzz.php
@@ -8,27 +8,55 @@ use Exception;
 
 class FizzBuzz
 {
+	private int $min = 0;
+
+	private int $max = 100;
+
+	private int $fizz = 3;
+
+	private int $buzz = 5;
+
+	private int $modulo = 0;
+
+	private string $outOfRange = 'Input is out of range';
+
+	private string $stringFizz = 'Fizz';
+
+	private string $stringBuzz = 'Buzz';
+
 	public function fizzBuzz(int $input): string | int
 	{
-		if (0 < $input) {
-			if (100 >= $input) {
-				if ($input % 3 === 0 && $input % 5 === 0) {
-					return 'FizzBuzz';
-				}
-				if ($input % 3 === 0) {
-					return 'Fizz';
-				}
-				if ($input % 5 === 0) {
-					return 'Buzz';
-				}
+		$this->validateInput($input);
 
-				return $input;
-			}
+		return $this->convertResult($input);
+	}
 
-			throw new Exception('Input is out of range');
-
-		} else {
-			throw new Exception('Input is out of range');
+	private function validateInput(int $input): void
+	{
+		if ($this->min >= $input || $this->max < $input) {
+			throw new Exception($this->outOfRange);
 		}
+	}
+
+	private function convertResult(int $input): string | int
+	{
+		if ($this->is($input, $this->fizz * $this->buzz)) {
+			return $this->stringFizz . $this->stringBuzz;
+		}
+
+		if ($this->is($input, $this->buzz)) {
+			return $this->stringBuzz;
+		}
+
+		if ($this->is($input, $this->fizz)) {
+			return $this->stringFizz;
+		}
+
+		return $input;
+	}
+
+	private function is(int $input, int $divisor): bool
+	{
+		return $input % $divisor === $this->modulo;
 	}
 }
