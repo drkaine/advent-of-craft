b34cf21e88cdf6b9a474fafc168176b0db443438
drkaine <duretkevin@live.fr>
2024-02-20 19:11:05 +0000

    Resolution of day 01

--- solution/php/day01/src/food/Food.php
+++ solution/php/day01/src/food/Food.php
@@ -10,19 +10,37 @@ use Ramsey\Uuid\UuidInterface;
 class Food
 {
 	public function __construct(
-		private readonly ?Carbon $expirationDate,
+		private readonly Carbon $expirationDate,
 		private readonly bool $approvedForConsumption,
 		private readonly ?UuidInterface $inspectorId,
 	) {
 	}
 
-	public function isEdible(?Carbon $now): bool
+	public function isEdible(Carbon $now): bool
 	{
-		if($this->expirationDate > $now && $this->approvedForConsumption && $this->inspectorId != null) {
-			return true;
-		} else {
+		if ($this->isEpired($now)) {
 			return false;
 		}
-		
+
+		if ($this->hasNotBeenInspected()) {
+			return false;
+		}
+
+		return $this->canBeConsummed();
+	}
+
+	private function isEpired(Carbon $now): bool
+	{
+		return $this->expirationDate <= $now;
+	}
+
+	private function hasNotBeenInspected(): bool
+	{
+		return null === $this->inspectorId;
+	}
+
+	private function canBeConsummed(): bool
+	{
+		return $this->approvedForConsumption;
 	}
 }
