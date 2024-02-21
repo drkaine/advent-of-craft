7b185020a7afad800fec963284d5f2f6d23cda9d
drkaine <duretkevin@live.fr>
2024-02-21 21:25:33 +0000

    Solution day 08
    Apply linter

--- solution/php/day08/src/Password.php
+++ solution/php/day08/src/Password.php
@@ -6,80 +6,76 @@ namespace App;
 
 class Password
 {
-    private int $minimumLenght = 8;
+	private int $minimumLenght = 8;
 
-    private string $regexLowercaseLetter = '/[a-z]/';
+	private string $regexLowercaseLetter = '/[a-z]/';
 
-    private string $regexCapitalLetter = '/[A-Z]/';
+	private string $regexCapitalLetter = '/[A-Z]/';
 
-    private string $regexNumber = '/[0-9]/';
+	private string $regexNumber = '/[0-9]/';
 
-    private string $regexSpecialCharacter = '/[.*#@\$%&]/';
+	private string $regexSpecialCharacter = '/[.*#@\$%&]/';
 
-    public function isValid(string $password): bool
-    {
-        if($this->haveTheRequireLenght($password)){
-            return false;
-        }
+	public function isValid(string $password): bool
+	{
+		if ($this->haveTheRequireLenght($password)) {
+			return false;
+		}
 
-        if($this->haveLowercaseLetter($password)){
-            return false;
-        }
+		if ($this->haveLowercaseLetter($password)) {
+			return false;
+		}
 
-        if($this->haveCapitalLetter($password)){
-            return false;
-        }
+		if ($this->haveCapitalLetter($password)) {
+			return false;
+		}
 
-        if($this->haveNumber($password)){
-            return false;
-        }
+		if ($this->haveNumber($password)) {
+			return false;
+		}
 
-        if($this->haveSpecialCharacter($password)){
-            return false;
-        }
+		if ($this->haveSpecialCharacter($password)) {
+			return false;
+		}
 
-        if($this->verifyNonAuthorizedCharacter($password)) {
-            return false;
-        }
+		return ! ($this->verifyNonAuthorizedCharacter($password));
+	}
 
-        return true;
-    }
+	private function haveTheRequireLenght(string $password): bool
+	{
+		return strlen($password) < $this->minimumLenght;
+	}
 
-    private function haveTheRequireLenght(string $password): bool
-    {
-        return strlen($password) < $this->minimumLenght;
-    }
+	private function haveLowercaseLetter(string $password): bool
+	{
+		return preg_match($this->regexLowercaseLetter, $password) !== 1;
+	}
 
-    private function haveLowercaseLetter(string $password): bool
-    {
-        return preg_match($this->regexLowercaseLetter, $password) !== 1;
-    }
+	private function haveCapitalLetter(string $password): bool
+	{
+		return preg_match($this->regexCapitalLetter, $password) !== 1;
+	}
 
-    private function haveCapitalLetter(string $password): bool
-    {
-        return preg_match($this->regexCapitalLetter, $password) !== 1;
-    }
+	private function haveNumber(string $password): bool
+	{
+		return preg_match($this->regexNumber, $password) !== 1;
+	}
 
-    private function haveNumber(string $password): bool
-    {
-        return preg_match($this->regexNumber, $password) !== 1;
-    }
+	private function haveSpecialCharacter(string $password): bool
+	{
+		return preg_match($this->regexSpecialCharacter, $password) !== 1;
+	}
 
-    private function haveSpecialCharacter(string $password): bool
-    {
-        return preg_match($this->regexSpecialCharacter, $password) !== 1;
-    }
+	private function verifyNonAuthorizedCharacter(string $password): bool
+	{
+		$passwordTest = preg_replace($this->regexLowercaseLetter, '', $password);
 
-    private function verifyNonAuthorizedCharacter(string $password): bool
-    {
-        $passwordTest = preg_replace($this->regexLowercaseLetter, '', $password);
+		$passwordTest = preg_replace($this->regexCapitalLetter, '', $passwordTest);
 
-        $passwordTest = preg_replace($this->regexCapitalLetter, '', $passwordTest);
+		$passwordTest = preg_replace($this->regexNumber, '', $passwordTest);
 
-        $passwordTest = preg_replace($this->regexNumber, '', $passwordTest);
+		$passwordTest = preg_replace($this->regexSpecialCharacter, '', $passwordTest);
 
-        $passwordTest = preg_replace($this->regexSpecialCharacter, '', $passwordTest);
-
-        return $passwordTest !== '';
-    }
+		return '' !== $passwordTest;
+	}
 }
--- solution/php/day08/tests/Unit/PasswordTest.php
+++ solution/php/day08/tests/Unit/PasswordTest.php
@@ -9,31 +9,31 @@ beforeEach(function (): void {
 });
 
 describe('Password need to return', function (): void {
-    test('false if he have less than 8 character', function (): void {
+	test('false if he have less than 8 character', function (): void {
 		expect($this->password->isValid('2345.Aa'))->toBe(false);
 	});
 
-    test('false if he have less than 1 lowercase letter', function (): void {
+	test('false if he have less than 1 lowercase letter', function (): void {
 		expect($this->password->isValid('123456.A'))->toBe(false);
 	});
 
-    test('false if he have less than 1 capital letter', function (): void {
+	test('false if he have less than 1 capital letter', function (): void {
 		expect($this->password->isValid('123456.a'))->toBe(false);
 	});
 
-    test('false if he have less than 1 number', function (): void {
+	test('false if he have less than 1 number', function (): void {
 		expect($this->password->isValid('bbbbb.Aa'))->toBe(false);
 	});
 
-    test('false if he have less than a special character in this list . * # @ $ % &', function (): void {
+	test('false if he have less than a special character in this list . * # @ $ % &', function (): void {
 		expect($this->password->isValid('123456Aa'))->toBe(false);
 	});
 
-    test('false if he have a character not in this list . * # @ $ % &', function (): void {
+	test('false if he have a character not in this list . * # @ $ % &', function (): void {
 		expect($this->password->isValid('123456.Aa '))->toBe(false);
 	});
 
-    test('true if it\'s a valid password', function (): void {
+	test('true if it\'s a valid password', function (): void {
 		expect($this->password->isValid('123456.Aa'))->toBe(true);
 	});
 });
