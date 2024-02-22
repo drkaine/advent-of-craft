7f743cb57604a9c0c99730ab98bf6a2687f8c559
drkaine <duretkevin@live.fr>
2024-02-22 10:58:56 +0000

    Refactoring day 08
    Rename test name for respected the convention namming

--- solution/php/day08/tests/Unit/PasswordTest.php
+++ solution/php/day08/tests/Unit/PasswordTest.php
@@ -8,32 +8,32 @@ beforeEach(function (): void {
 	$this->password = new Password;
 });
 
-describe('Password need to return', function (): void {
-	test('false if he have less than 8 character', function (): void {
+describe('Password need to be', function (): void {
+	test('invalid if he have less than 8 character', function (): void {
 		expect($this->password->isValid('2345.Aa'))->toBe(false);
 	});
 
-	test('false if he have less than 1 lowercase letter', function (): void {
+	test('invalid if he have less than 1 lowercase letter', function (): void {
 		expect($this->password->isValid('123456.A'))->toBe(false);
 	});
 
-	test('false if he have less than 1 capital letter', function (): void {
+	test('invalid if he have less than 1 capital letter', function (): void {
 		expect($this->password->isValid('123456.a'))->toBe(false);
 	});
 
-	test('false if he have less than 1 number', function (): void {
+	test('invalid if he have less than 1 number', function (): void {
 		expect($this->password->isValid('bbbbb.Aa'))->toBe(false);
 	});
 
-	test('false if he have less than a special character in this list . * # @ $ % &', function (): void {
+	test('invalid if he have less than a special character in this list . * # @ $ % &', function (): void {
 		expect($this->password->isValid('123456Aa'))->toBe(false);
 	});
 
-	test('false if he have a character not in this list . * # @ $ % &', function (): void {
+	test('invalid if he have a character not in this list . * # @ $ % &', function (): void {
 		expect($this->password->isValid('123456.Aa '))->toBe(false);
 	});
 
-	test('true if it\'s a valid password', function (): void {
+	test('valid if it\'s a valid password', function (): void {
 		expect($this->password->isValid('123456.Aa'))->toBe(true);
 	});
 });
